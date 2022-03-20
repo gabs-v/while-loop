6703cc84e52ba6f102362bc605cd421e116daf2b
gabs-v <gabyvazquez8297@gmail.com>
2022-03-20 11:12:03 -0700

    first commit

---
+++ while_loop.py
@@ -0,0 +1,9 @@
+#Learning and getting familiar with while loops! :) 
+
+i = 1 #creating an integer (we dont need one for while loops but just for this example, we're using it :)
+while i <= 10: #specifying out loop condition. This will keep on looping as long as this is true.
+    print(i)
+    i += 1 #automatically adds one...kinda like ++ in Javascript
+
+print("Done with loop") #executes when done looping through
+#after every execution of the loop it will go back up and check to see if it's still true
